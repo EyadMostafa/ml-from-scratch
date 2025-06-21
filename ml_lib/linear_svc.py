4dd12e71b65e4a75871975230793861ddfa739f3
EyadMostafa <eyadbu3@gmail.com>
2025-06-21 17:12:10 +0300

    changed the encaplsualtion of some variables and methods and teaked the model to not store X and y

--- ml_lib/linear_svc.py
+++ ml_lib/linear_svc.py
@@ -4,47 +4,63 @@ from utils.optimizers import gradient_descent
 from utils.helpers import validate_transform_input
 
 class LinearSVC:
-    def __init__(self, C=1.0, learning_rate=0.01, max_iter=1000, tol=1e-4):
+    """
+    Linear Support Vector Classifier using hinge loss and gradient descent optimization.
+
+    Parameters:
+    -----------
+    C : float
+        Regularization parameter.
+    learning_rate : float
+        Learning rate for gradient descent.
+    max_iter : int
+        Maximum number of iterations for gradient descent.
+    tol : float
+        Tolerance for stopping criteria based on gradient norm.
+    """
+    def __init__(self, C=1.0, learning_rate=0.01, max_iter=1000, tol=1e-4) -> None:
         self.C = C
         self.learning_rate = learning_rate
         self.max_iter = max_iter
         self.tol = tol
         self._w = None
         self._b = None
-        self._X = None
-        self._y = None
 
-    _validate_transform_input = staticmethod(validate_transform_input)
-    _gradient_descent = staticmethod(gradient_descent)
+    __validate_transform_input = staticmethod(validate_transform_input)
+    __gradient_descent = staticmethod(gradient_descent)
 
-    def _hinge_loss_gradient(self, params):
+    def _hinge_loss_gradient(self, params, X, y):
         w, b = params
 
-        margin = self._X @ w + b
+        margin = X @ w + b
         mask = margin <= 1
 
-        dw = w - self.C * self._y[mask] @ self._X[mask]
-        db = -self.C * np.sum(self._y[mask])
+        dw = w - self.C * y[mask] @ X[mask]
+        db = -self.C * np.sum(y[mask])
 
         return np.array([dw, db], dtype=object)
 
     def fit(self, X, y):
-        self._X, self._y = self._validate_transform_input(X, y)
-        self._y = np.where(self._y <= 0, -1, 1) 
-        n = self._X.shape[1]
+        X, y = self.__validate_transform_input(X, y)
+        y = np.where(y <= 0, -1, 1) 
+        n = X.shape[1]
 
         self._w = np.zeros(n)
         self._b = 0.0
 
-        self._w, self._b = self._gradient_descent(
+        self._w, self._b = self.__gradient_descent(
             gradient_fn=self._hinge_loss_gradient,
             params=[self._w, self._b],
             learning_rate=self.learning_rate,
             max_iter=self.max_iter,
-            tol=self.tol
+            tol=self.tol,
+            features=X,
+            labels=y
         )
         
     def predict(self, X):
-        X, _ = self._validate_transform_input(X)
+        if self._w is None or self._b is None:
+            raise ValueError("Model has not been fitted yet. Call 'fit' before 'predict'.")
+        X, _ = self.__validate_transform_input(X)
         predictions = X @ self._w + self._b
         return np.where(predictions >= 0, 1, 0)
--- utils/optimizers.py
+++ utils/optimizers.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def gradient_descent(gradient_fn, params, learning_rate=0.01, max_iter=1000, tol=1e-4):
+def gradient_descent(gradient_fn, params, features, labels, learning_rate=0.01, max_iter=1000, tol=1e-4):
     for _ in range(max_iter):
-        gradients = gradient_fn(params)
+        gradients = gradient_fn(params, features, labels)
         combined_grads = np.concatenate([gradients[0].ravel(), gradients[1].ravel()])
         if np.linalg.norm(combined_grads) < tol:
             break
